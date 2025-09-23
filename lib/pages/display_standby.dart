00cb6e6abd1a8132dbd7a29c2d52d0dba35114c4
hischan12 <hischan12@handong.ac.kr>
2025-09-23 23:28:48 +0900

    display_standby page asset 참조 수정

--- lib/pages/display_standby.dart
+++ lib/pages/display_standby.dart
@@ -1,20 +1,15 @@
 // lib/pages/display_quiz_page.dart
 import 'package:flutter/material.dart';
-import '../../main.dart';   
+import '../../main.dart';
 
 class DisplayStandByPage extends StatelessWidget {
-  const DisplayStandByPage ({Key? key}) : super(key: key);
+  const DisplayStandByPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 246, 250, 255),
-      body: Center(
-        child: Image.asset(
-          'logo_bird_standby.png',
-          ),
-        ),
-      );
-    
+      body: Center(child: Image.asset('assets/logo_bird_standby.png')),
+    );
   }
 }
